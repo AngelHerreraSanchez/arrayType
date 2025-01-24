ec33f149447205a007b820c23ce27f5bb1504aff
herreang000 <herreang000@warren.k12.in.us>
2025-01-24 11:22:25 -0500

    final

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        class ResizableArray<T> {
+            private T[] array;
+            private int size;
+
+            @SuppressWarnings("unchecked")
+            public ResizableArray() {
+                array = (T[]) new Object[10];
+                size = 0;
+            }
+
+            Arrays.sort(T);
+
+
+            System.out.print("");
+            for(int size : T) {
+                System.out.print(size + " ");
+            }
+
+
+
+
+        }
+    }
+}
